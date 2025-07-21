ef61a9f8d9c9254b098922cca096c758f0dd2bb8
Kushalsingh Chauhan <c2kushalsingh@gmail.com>
2025-07-21 12:30:30 +0530

    added api docs using swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle("Crud apis")
+    .setDescription("API docs for nestjs API")
+    .setVersion("1.0")
+    .addTag("crud")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 5000);
   console.log("server started in port 5000")
 }
