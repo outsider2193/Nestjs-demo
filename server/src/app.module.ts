82eceb5649b90282c99f8ff4e9fed5589354056d
Kushalsingh Chauhan <c2kushalsingh@gmail.com>
2025-07-29 18:35:24 +0530

    .

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -28,7 +28,7 @@ import { JwtModule } from '@nestjs/jwt';
       type: 'mysql',
       host: "localhost",
       port: 3306,
-      username: "root",
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [User],
