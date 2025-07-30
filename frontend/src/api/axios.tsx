1f3592727f24d14ca88990a32d2a6792d5d15e9d
Kushalsingh Chauhan <c2kushalsingh@gmail.com>
2025-07-30 18:01:24 +0530

    .

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({baseURL:"http://3.109.153.109:5000"})
 
 
 
